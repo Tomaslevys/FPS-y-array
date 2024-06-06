41703d3dbc0835791738e546c7d2b400aaca18af
Tomaslevys <141672471+Tomaslevys@users.noreply.github.com>
2024-06-06 10:42:02 -0300

    proyecto terminado

--- Assets/alimentoScript.cs
+++ Assets/alimentoScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class alimentoScript : MonoBehaviour
 {
+    public int valorAlimenticio;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/personajeScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personajeScript : MonoBehaviour
+{
+    public int nivelHambre;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void tomarAlimento(int ValorAlimentario)
+    {
+        nivelHambre -= ValorAlimentario;
+        if (nivelHambre<= 0 )
+        {
+            nivelHambre = 0;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        alimentoScript alimento;
+        alimento = other.GetComponent<alimentoScript>();
+        if (alimento)
+        {
+            tomarAlimento(alimento.valorAlimenticio);
+            Destroy(other.gameObject);
+        }
+    }
+}
